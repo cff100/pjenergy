b9776645abe5d763fc27fde8a965074e9912234f
Caio_Felix <caiofelix190799@gmail.com>
2024-11-18 15:50:03 -0300

    Update simplifica.py

--- src/simplifica.py
+++ src/simplifica.py
@@ -1,29 +1,32 @@
-# Para facilitar a escolha da plataforma
+from .usuario import valores_nao_aceitos
 
+
+# Para facilitar a escolha da plataforma
 def simplifica_plat(plataforma):
 
-  #print("Função ativada: simplifica_funcao \n")
-
+  valores_nao_aceitos(plataforma, ["1", "2", "3", "4", "5", "6", "7", "8", "9",
+                                   'NAMORADO 2 (PNA-2)', 'PETROBRAS 26 (P-26)',
+                                   'PETROBRAS 32 (P-32)', 'PETROBRAS 37 (P-37)',
+                                   'PETROBRAS IX', 'PETROBRAS XIX', 'PETROBRAS XXXIII',
+                                   'VERMELHO 1 (PVM-1)', 'VERMELHO 2 (PVM-2)'])
+  
   if plataforma == '1':
-    return 'NAMORADO 2 (PNA-2)', None
+    return 'NAMORADO 2 (PNA-2)'
   elif plataforma == '2':
-    return 'PETROBRAS 26 (P-26)', None
+    return 'PETROBRAS 26 (P-26)'
   elif plataforma == '3':
-    return 'PETROBRAS 32 (P-32)', None
+    return 'PETROBRAS 32 (P-32)'
   elif plataforma == '4':
-    return 'PETROBRAS 37 (P-37)', None
+    return 'PETROBRAS 37 (P-37)'
   elif plataforma == '5':
-    return 'PETROBRAS IX', None
+    return 'PETROBRAS IX'
   elif plataforma == '6':
-    return 'PETROBRAS XIX', None
+    return 'PETROBRAS XIX'
   elif plataforma == '7':
-    return 'PETROBRAS XXXIII', None
+    return 'PETROBRAS XXXIII'
   elif plataforma == '8':
-    return 'VERMELHO 1 (PVM-1)', None
+    return 'VERMELHO 1 (PVM-1)'
   elif plataforma == '9':
-    return 'VERMELHO 2 (PVM-2)', None
-  elif isinstance(plataforma, list):
-    mensagem_erro = "Lista de plataformas não é aceito \n"
-    return None, mensagem_erro
+    return 'VERMELHO 2 (PVM-2)'
   else:
-    return plataforma, None
+    return plataforma
