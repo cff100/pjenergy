fc575a5efe4b40c568d17093e6e82c22a01fcd33
cff100 <caiofelix190799@gmail.com>
2024-11-19 20:19:54 +0000

    Atualizações no projeto

--- src/usuario.py
+++ src/usuario.py
@@ -12,11 +12,11 @@ def valores_nao_aceitos(valor_escolhido, valores_aceitos, dica = False, nome_var
   #print(dica)
   #print(f"Checando valor: {valor_escolhido}")
   if valor_escolhido not in valores_aceitos:
-    print("ERRO: Valor não aceito")
     if dica == True:
       print(f"ERRO: Valor não aceito para {nome_variavel}")
       print(f"Valores aceitos: {valores_aceitos} \n")
     else:
+      print("ERRO: Valor não aceito")
       print('\n')
     return False
   else:
