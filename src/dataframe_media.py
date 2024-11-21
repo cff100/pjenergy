8520fb63f7178f7f09a608b34eed76ad1f980e4d
cff100 <caiofelix190799@gmail.com>
2024-11-21 20:39:15 +0000

    Atualizações no projeto

---
+++ src/dataframe_media.py
@@ -0,0 +1,19 @@
+def dataframe_media(df, estacoes_separadas):
+  '''if estacoes_separadas == False:
+    categorias_agrupar = ['Estação_do_Ano', 'Altitude_m', 'Horário_Brasília']
+    colunas_ordem = ['Nível_de_Pressão_hPa', 'Altitude_m', 'Estação_do_Ano',
+                     'Horário_Brasília', 'Horário_UTC', 'Velocidade_Vento_u_m/s',
+                     'Velocidade_Vento_v_m/s', 'Velocidade_Vento_resultante_m/s', 'Temperatura_K', 'Temperatura_C']
+
+    df_media = df.groupby(categorias_agrupar).agg({
+    'Velocidade_Vento_resultante_m/s': 'mean',
+    'Velocidade_Vento_u_m/s': 'mean',
+    'Velocidade_Vento_v_m/s': 'mean',
+    'Temperatura_K': 'mean',
+    'Temperatura_C': 'mean',
+    'Nível_de_Pressão_hPa': 'first',
+    'Horário_UTC': 'first',
+    'Data': 'first',
+    }).reset_index()'''
+
+  return 'teste'
