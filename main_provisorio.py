57be45451ca88d3b7f5ca28be70ff2c4c6a7a34e
cff100 <caiofelix190799@gmail.com>
2024-11-22 14:17:51 +0000

    Atualizações no projeto

--- main_provisorio.py
+++ main_provisorio.py
@@ -2,4 +2,4 @@ import src.usuario as us
 
 def main(perguntas = True, variavel = "Ambos", modo = "Original", componente_velocidade = "Resultante", plataforma = "7", estacao = "Geral", indicador = "Média", data = None, ano = "Todos"):
   a = us.argumentos_usuario(perguntas, variavel, modo, componente_velocidade, plataforma, estacao, indicador, data, ano)
-  return a['variavel']
+  return a
