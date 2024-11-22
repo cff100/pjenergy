8228a0daefb0c56b573915152bf09537f5f95551
cff100 <caiofelix190799@gmail.com>
2024-11-22 16:39:21 +0000

    Atualizações no projeto

--- main_provisorio.py
+++ main_provisorio.py
@@ -1,5 +1,8 @@
 import src.usuario as us
+import src.numero_linhas_colunas as nlc
 
 def main(perguntas = True, variavel = "Ambos", modo = "Original", componente_velocidade = "Resultante", plataforma = "7", estacao = "Geral", indicador = "Média", data = None, ano = "Todos"):
+  
   dicionario_argumentos = us.argumentos_usuario(perguntas, variavel, modo, componente_velocidade, plataforma, estacao, indicador, data, ano)
-  return dicionario_argumentos
+  ncl.linhas_colunas(dicionario_argumentos)
+  
