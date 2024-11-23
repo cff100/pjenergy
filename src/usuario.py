536d6ae1c51ebc22d15d7127ad4247bb04cc1170
cff100 <caiofelix190799@gmail.com>
2024-11-23 16:50:58 +0000

    Atualizações no projeto

--- src/usuario.py
+++ src/usuario.py
@@ -413,7 +413,8 @@ def escolha_direta_usuario(variavel, modo, componente_velocidade, plataforma, es
       if variavel != 'Velocidade':
         variavel = 'Velocidade'
         print(f"- Variável precisa ser 'Velocidade' ou 'Temperatura'. Variável foi alterada automaticamente para {variavel} \n")
-  elif estacao == None: # estacao == None
+  else:
+    print(estacao)
     if data == None and indicador != 'Sem_filtros':
       print("Devido à escolha de data e estacao como None:")
       indicador = 'Sem_filtros'
