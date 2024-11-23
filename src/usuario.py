67e296d4d2d6ce0f099adc3e6a32e07af73940d4
cff100 <caiofelix190799@gmail.com>
2024-11-23 17:10:31 +0000

    Atualizações no projeto

--- src/usuario.py
+++ src/usuario.py
@@ -381,7 +381,7 @@ def escolha_direta_usuario(variavel, modo, componente_velocidade, plataforma, es
     return None
   if data != None:
     df = df[df['Data'] == data]
-    df.drop(columns=['Data'], inplace = True).reset_index()
+    df.drop(columns=['Data'], inplace = True).reset_index(drop=True)
 
 
   variavel = valores_nao_aceitos(variavel, ["Velocidade", "Temperatura", "Ambos"], dica = True, nome_variavel = 'variavel')
