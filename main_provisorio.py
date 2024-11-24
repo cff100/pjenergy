218d14ad1fa5e1176d18737773a960e0257609dc
cff100 <caiofelix190799@gmail.com>
2024-11-24 00:37:41 +0000

    Atualizações no projeto

--- main_provisorio.py
+++ main_provisorio.py
@@ -4,7 +4,6 @@ import src.numero_linhas_colunas as nlc
 def main(perguntas = True, variavel = "Ambas", modo = "Original", componente_velocidade = "Resultante", plataforma = "7", estacao = "Geral", indicador = "Média", data = None, ano = "Todos"):
 
   dicionario_argumentos = us.argumentos_usuario(perguntas, variavel, modo, componente_velocidade, plataforma, estacao, indicador, data, ano)
-  print(dicionario_argumentos)
   if dicionario_argumentos['data'] == None and dicionario_argumentos['estacao'] == None:
     print('Não é possível criar um gráfico com essa combinação de data e estação \n Dataframe gerado:')
     return dicionario_argumentos['df']
