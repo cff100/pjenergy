57571836bd50200283a8ec57ec0895df36fecdfd
cff100 <caiofelix190799@gmail.com>
2024-11-24 21:07:11 +0000

    Atualizações no projeto

--- src/iteracao_graficos.py
+++ src/iteracao_graficos.py
@@ -27,7 +27,7 @@ def iteracao_grafico(dicionario_argumentos):
 
     if estacao == "Todas":
       for est in list(df_hora['Estação_do_Ano'].unique()):
-        print(es)
+        print(est)
 
     # Coluna de alturas para o eixo Y do gráfico
     Y = df_hora['Altitude_m']
