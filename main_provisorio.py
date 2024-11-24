e13025e7ebb15c704c0ee3f3ab3c87be73fdcb41
cff100 <caiofelix190799@gmail.com>
2024-11-24 00:45:21 +0000

    Atualizações no projeto

--- main_provisorio.py
+++ main_provisorio.py
@@ -8,7 +8,7 @@ def main(perguntas = True, variavel = "Ambas", modo = "Original", componente_vel
   print(dicionario_argumentos['data'])
   if dicionario_argumentos['data'] == None and dicionario_argumentos['estacao'] == None:
     print('Não é possível criar um gráfico com essa combinação de data e estação \n Dataframe gerado:')
-    return dicionario_argumentos['df']
+    return dicionario_argumentos
   a = nlc.linhas_colunas(dicionario_argumentos)
 
   return a
