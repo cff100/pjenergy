8193245cb89ebc24bd52458f18b7fdd37584b06e
cff100 <caiofelix190799@gmail.com>
2024-11-24 01:03:16 +0000

    Atualizações no projeto

--- main_provisorio.py
+++ main_provisorio.py
@@ -4,9 +4,12 @@ import src.numero_linhas_colunas as nlc
 def main(perguntas = True, variavel = "Ambas", modo = "Original", componente_velocidade = "Resultante", plataforma = "7", estacao = "Geral", indicador = "Média", data = None, ano = "Todos"):
 
   dicionario_argumentos = us.argumentos_usuario(perguntas, variavel, modo, componente_velocidade, plataforma, estacao, indicador, data, ano)
+
   if not isinstance(dicionario_argumentos, dict):
     print('Não é possível criar um gráfico com essa combinação de data e estação \n Dataframe gerado:')
     return dicionario_argumentos
-  n_lin, n_col = nlc.linhas_colunas(dicionario_argumentos)
 
-  return n_lin, n_col
+  n_lin, n_col = nlc.linhas_colunas(dicionario_argumentos)
+  fig, axs = plt.subplots(n_lin, n_col, figsize=(9*n_col, 6*n_lin))
+
+  return axs
