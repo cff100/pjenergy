c70eab8cb60fe404f39cec5fe62e45ac047406fc
cff100 <caiofelix190799@gmail.com>
2024-11-27 17:48:01 +0000

    Atualizações no projeto

--- src/dataframe_media.py
+++ src/dataframe_media.py
@@ -16,7 +16,7 @@ def dataframe_media(df, estacoes_separadas, estacao):
     }).reset_index()
 
   else:
-    if estacao == 'Geral':
+    '''if estacao == 'Geral':
       categorias_agrupar = ['Altitude_m', 'Horário_Brasília']
       colunas_ordem = ['Nível_de_Pressão_hPa', 'Altitude_m', 
                       'Horário_Brasília', 'Horário_UTC', 'Velocidade_Vento_u_m/s',
@@ -30,24 +30,24 @@ def dataframe_media(df, estacoes_separadas, estacao):
       'Temperatura_C': 'mean',
       'Nível_de_Pressão_hPa': 'first',
       'Horário_UTC': 'first'
-      }).reset_index()
+      }).reset_index()'''
 
-    else:
-      categorias_agrupar = ['Altitude_m', 'Horário_Brasília']
-      colunas_ordem = ['Nível_de_Pressão_hPa', 'Altitude_m', 'Estação_do_Ano',
-                      'Horário_Brasília', 'Horário_UTC', 'Velocidade_Vento_u_m/s',
-                      'Velocidade_Vento_v_m/s', 'Velocidade_Vento_resultante_m/s', 'Temperatura_K', 'Temperatura_C']
+    '''else:'''
+    categorias_agrupar = ['Altitude_m', 'Horário_Brasília']
+    colunas_ordem = ['Nível_de_Pressão_hPa', 'Altitude_m', 'Estação_do_Ano',
+                    'Horário_Brasília', 'Horário_UTC', 'Velocidade_Vento_u_m/s',
+                    'Velocidade_Vento_v_m/s', 'Velocidade_Vento_resultante_m/s', 'Temperatura_K', 'Temperatura_C']
 
-      df_media = df.groupby(categorias_agrupar).agg({
-      'Velocidade_Vento_resultante_m/s': 'mean',
-      'Velocidade_Vento_u_m/s': 'mean',
-      'Velocidade_Vento_v_m/s': 'mean',
-      'Temperatura_K': 'mean',
-      'Temperatura_C': 'mean',
-      'Nível_de_Pressão_hPa': 'first',
-      'Horário_UTC': 'first',
-      'Estação_do_Ano': 'first'
-      }).reset_index()
+    df_media = df.groupby(categorias_agrupar).agg({
+    'Velocidade_Vento_resultante_m/s': 'mean',
+    'Velocidade_Vento_u_m/s': 'mean',
+    'Velocidade_Vento_v_m/s': 'mean',
+    'Temperatura_K': 'mean',
+    'Temperatura_C': 'mean',
+    'Nível_de_Pressão_hPa': 'first',
+    'Horário_UTC': 'first',
+    'Estação_do_Ano': 'first'
+    }).reset_index()
 
 
   return df_media[colunas_ordem]
