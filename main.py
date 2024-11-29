c96a292a925beb3a02690e07bebe1e8c28a48f0b
cff100 <caiofelix190799@gmail.com>
2024-11-29 20:34:24 +0000

    Atualizações no projeto

--- main.py
+++ main.py
@@ -27,6 +27,16 @@ def main(perguntas = True, variavel = "Ambas", modo = "Original", componente_vel
 
   itg.iteracao_grafico(dicionario_argumentos, axs)
 
+  
+
+
+  #return x
+
+
+def criacao_interpolacao(perguntas = True, variavel = "Ambas", modo = "Original", componente_velocidade = "Resultante", plataforma = "7", estacao = "Geral", indicador = "Média", data = None, ano = "Todos"):
+
+  dicionario_argumentos = us.argumentos_usuario(perguntas, variavel, modo, componente_velocidade, plataforma, estacao, indicador, data, ano)
+  
   if dicionario_argumentos['data'] == None and dicionario_argumentos['componente_velocidade'] in ['Resultante', None]:
     ti.interpolacao(dicionario_argumentos)
   else:
@@ -34,6 +44,3 @@ def main(perguntas = True, variavel = "Ambas", modo = "Original", componente_vel
       print('Quando uma data específica é escolhida, não é gerada uma tabela de interpolação')
     if dicionario_argumentos['componente_velocidade'] not in ['Resultante', None]:
       print('Não é gerada uma tabela de interpolação para tabelas de componentes específicas')
-
-
-  #return x
