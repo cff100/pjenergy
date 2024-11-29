1d16617579a9bcd60a78b517a87eeb20d84411ad
cff100 <caiofelix190799@gmail.com>
2024-11-29 23:01:47 +0000

    Atualizações no projeto

--- src/perfis/usuario.py
+++ src/perfis/usuario.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime
 from .simplifica import simplifica_plat
-from .valores_nao_aceitos import valores_nao_aceitos
+from src.perfis.valores_nao_aceitos import valores_nao_aceitos
 from .dataframe_media import dataframe_media
 
 
