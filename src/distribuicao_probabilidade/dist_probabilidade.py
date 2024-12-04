888777f6f64dc00de41472431634e7ee29e9c6f5
cff100 <caiofelix190799@gmail.com>
2024-12-04 16:21:08 +0000

    Atualizações no projeto

--- src/distribuicao_probabilidade/dist_probabilidade.py
+++ src/distribuicao_probabilidade/dist_probabilidade.py
@@ -7,9 +7,46 @@ import numpy as np
 horarios = ['03:00', '09:00', '15:00', '21:00']
 plataforma_escolhida = "PETROBRAS XXXIII"
 
-def plot_weibull_velocidade(pressao, estacao, ano):
+def plot_weibull_velocidade(pressao, estacao, ano, horario):
 
-  # Lista para armazenar os DataFrames temporariamente
+
+  # Lista de caminhos para os arquivos CSV
+  arquivos_csv = ['/content/pjenergy/data/dados_interpolados/df_interpolado_Verao.csv', '/content/pjenergy/data/dados_interpolados/df_interpolado_Outono.csv', '/content/pjenergy/data/dados_interpolados/df_interpolado_Inverno.csv', '/content/pjenergy/data/dados_interpolados/df_interpolado_Primavera.csv']
+
+  # Lista para armazenar os DataFrames
+  dataframes = [pd.read_csv(arquivo) for arquivo in arquivos_csv]
+
+  # Concatenar todos os DataFrames em um único
+  df_combinado = pd.concat(dataframes, ignore_index=True)
+
+  if pressao not in ['Todas', '0']:
+    df_combinado = df_combinado[df_combinado['Nível_de_Pressão_hPa'] == float(pressao)]
+  else:
+    if pressao == '0':
+      pressao = 'Todas'
+
+  if estacao not in ['Todas', '0']:
+    df_combinado = df_combinado[df_combinado['Estação_do_Ano'] == estacao]
+  else:
+    if estacao == '0':
+      estacao = 'Todas'
+
+  if ano not in ['Todos', '0']:
+    df_combinado['Data'] = pd.to_datetime(df_combinado['Data'])
+    df_combinado = df_combinado[df_combinado['Data'].dt.year == int(ano)]
+  else:
+    if ano == '0':
+      ano = 'Todos'
+  
+  if horario not in ['Todos', '0']:
+    df_combinado = df_combinado[df_combinado['Horário_Brasília'] == horario]
+  else:
+    if horario == '0':
+      horario = 'Todos'
+
+  print(df_combinado)
+
+  '''# Lista para armazenar os DataFrames temporariamente
   dfs = []
 
   if estacao == "Verão":
@@ -131,10 +168,10 @@ def plot_weibull_velocidade(pressao, estacao, ano):
   # Salvar a tabela em um arquivo CSV, se desejado
   tabela_probabilidades.to_csv(f'Velocidade_Tabela_Probabilidades_Weibull.csv', index=False)
 
-  return tabela_probabilidades
+  return tabela_probabilidades'''
 
 
-def usuario_weibull_velocidade(perguntas, pressao, estacao, ano):
+def usuario_weibull_velocidade(perguntas, pressao, estacao, ano, horario):
 
   '''Inicia a busca pelos argumentos do usuário'''
 
@@ -142,11 +179,12 @@ def usuario_weibull_velocidade(perguntas, pressao, estacao, ano):
     pressao = input('Qual pressão deseja observar (em HPa)? Escolha um número inteiro entre 972 e 1000. Escreva Todas ou 0 para não filtrar nenhuma pressão específica. \n')
     estacao = input('Qual estação deseja observar? Escolha entre Verão, Outono, Inverno ou Primavera. Escreva Todas ou 0 para não filtrar nenhuma estação específica. \n')
     ano = input('Qual ano deseja observar? Escolha um número inteiro entre 2010 e 2023. Escreva Todos ou 0 para não filtrar nenhum ano específico. \n')
+    horario = input('Qual horário deseja observar? Escolha entre 03:00, 09:00, 15:00 ou 21:00. Escreva Todos ou 0 para não filtrar nenhum horário específico. \n')
 
   else:
     pass
 
-  tabela = plot_weibull_velocidade(pressao, estacao, ano)
+  tabela = plot_weibull_velocidade(pressao, estacao, ano, horario)
 
 
   return tabela
