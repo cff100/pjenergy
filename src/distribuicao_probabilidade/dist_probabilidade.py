f81460540725a6cf135210c1d101d102e3560dc8
cff100 <caiofelix190799@gmail.com>
2024-12-04 13:44:26 +0000

    Atualizações no projeto

--- src/distribuicao_probabilidade/dist_probabilidade.py
+++ src/distribuicao_probabilidade/dist_probabilidade.py
@@ -119,6 +119,13 @@ def plot_weibull_velocidade(pressao, estacao, ano):
 
   # Exibir a tabela de probabilidades
   #print(tabela_probabilidades)
+  # Verificar se a soma da densidade de probabilidade está próxima de 1 para cada horário
+  for horario in horarios:
+    prob_sum = tabela_probabilidades[tabela_probabilidades['Horário'] == horario]['Densidade de Probabilidade'].sum()
+    if np.isclose(prob_sum, 1, atol=1e-6):  # atol define a margem de erro aceitável
+        print(f'A soma das probabilidades para o horário {horario} está correta: {prob_sum}')
+    else:
+        print(f'⚠️ A soma das probabilidades para o horário {horario} não é 1: {prob_sum}')
 
 
   # Salvar a tabela em um arquivo CSV, se desejado
