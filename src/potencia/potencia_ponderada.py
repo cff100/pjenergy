e4211d0342724249523a91dc92809ffc60bddb80
cff100 <caiofelix190799@gmail.com>
2024-12-06 12:47:58 +0000

    Atualizações no projeto

--- src/potencia/potencia_ponderada.py
+++ src/potencia/potencia_ponderada.py
@@ -93,6 +93,8 @@ def pond_potencia(df_mestre, pressao_lista, estacao_lista, ano_lista, horario_li
     
     df = df.sort_values(by='Velocidade_Vento_resultante_m/s')
     print(df)
+    print('\n')
+    print(df_mestre[0])
     '''# Identificar estilo com base em critérios (substitua conforme necessário)
     estacao = estacao_lista[idx] if idx < len(estacao_lista) else 'Outono'
     horario = horario_lista[idx] if idx < len(horario_lista) else '15:00'
