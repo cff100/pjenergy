c2ae2ac0e7f442f5cbba2be05ce8045560fd7991
cff100 <caiofelix190799@gmail.com>
2024-12-07 16:56:19 +0000

    Atualizações no projeto

--- src/potencia/grafico_potencia_ponderada.py
+++ src/potencia/grafico_potencia_ponderada.py
@@ -85,7 +85,7 @@ def pond_potencia(df_mestre, pressao_lista, estacao_lista, ano_lista, horario_li
 
   lista_contagem = []
 
-  print(f'df_mestre: {df_mestre}')
+  #print(f'df_mestre: {df_mestre}')
   # Iterar sobre os DataFrames na coluna do DataFrame mestre
   for idx, df in enumerate(df_mestre['Dataframe_Probabilidade']):
     # Calcular a potência
@@ -98,7 +98,7 @@ def pond_potencia(df_mestre, pressao_lista, estacao_lista, ano_lista, horario_li
     #print('\n')
     #print(df.tail(40))
 
-    print(f'df: {df}')
+    #print(f'df: {df}')
     # Identificar a estação e o horário correspondentes
     estacao = df_mestre.loc[idx, 'Estação']
     horario = df_mestre.loc[idx, 'Horário']
@@ -188,6 +188,8 @@ def pond_potencia(df_mestre, pressao_lista, estacao_lista, ano_lista, horario_li
       lista_contagem.append(variavel_contada)
       handles.append(line)
       labels.append(label)
+
+  df_mestre['Dataframe_Probabilidade']
 
   # Configurar o gráfico
   ax.set_title(titulo)
