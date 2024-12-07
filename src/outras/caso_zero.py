429ffb27d469f2afdf9198844a029fc5519ce1c7
cff100 <caiofelix190799@gmail.com>
2024-12-07 11:23:55 +0000

    Atualizações no projeto

--- src/outras/caso_zero.py
+++
@@ -1,6 +0,0 @@
-def zero_para_todos(variavel, nome_variavel):
-  if nome_variavel in ['Estação', 'Pressão']:
-    variavel = 'Todas'
-  elif nome_variavel in ['Ano', 'Horário']:
-    variavel = 'Todos'
-  return variavel
