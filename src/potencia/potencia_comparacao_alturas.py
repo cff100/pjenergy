f259e5bcf00c0a60a7a0f22725cb3f2ac8dc13b2
cff100 <caiofelix190799@gmail.com>
2024-12-09 12:41:34 +0000

    Atualizações no projeto

--- src/potencia/potencia_comparacao_alturas.py
+++ src/potencia/potencia_comparacao_alturas.py
@@ -1,25 +1,46 @@
 import pjenergy.main as mp
 import pandas as pd
+from scipy.integrate import simps
 
 def potencia_altura(perguntas, l_vel_inf, l_vel_sup):
+
+  # Parâmetros iniciais
+  rho = 1.225  # Densidade do ar (kg/m^3)
+  A = 1       # Área da unitária da turbina (m^2)
 
   df_mestre_limitado = pd.DataFrame()
   for an in range(2021, 2024):
     for est in ['Verão', 'Outono', 'Inverno', 'Primavera']:
       df_mestre_limitado = pd.concat([df_mestre_limitado, mp.pot(perguntas = False, pressao = 'Todas', estacao = est, ano = an, horario = 'Todos')])
 
-  for idx, df in enumerate(df_mestre['Dataframe_Probabilidade']):
-    df_mestre_limitado['Dataframe_Probabilidade'].iloc[idx] = df.loc[
+  for idx, df in enumerate(df_mestre_limitado['Dataframe_Probabilidade']):
+
+    df = df.loc[
         (df['Velocidade_Vento_resultante_m/s'] >= l_vel_inf) &
         (df['Velocidade_Vento_resultante_m/s'] <= l_vel_sup)
     ]
 
-  '''potencia_media = simps(df['Potência_Ponderada'], df['Velocidade_Vento_resultante_m/s'])
-  df_mestre.loc[idx, 'Potência_Média'] = potencia_media
-  print(f'Potência Ponderada Média: {potencia_media} kW/m^2')
+    # Calcular a potência
+    df['Potência'] = 0.5 * rho * A * (df['Velocidade_Vento_resultante_m/s'] ** 3) / 10**3  # Em kW
+    # Calcular a potência ponderada
+    df['Potência_Ponderada'] = df['Potência'] * df['Densidade_de_Probabilidade']
 
-  #print(df_mestre['Dataframe_Probabilidade'][0]['Velocidade_Vento_resultante_m/s'])
-  potencia_media_total = df_mestre['Potência_Média'].sum()
-  print(f'Potência Total: {potencia_media_total} kW/m^2')'''
+    df = df.sort_values(by='Velocidade_Vento_resultante_m/s')
 
-  print(df_mestre_limitado)
+    df_mestre_limitado['Dataframe_Probabilidade'].iloc[idx] = df
+
+    potencia_media_local = simps(df['Potência_Ponderada'], df['Velocidade_Vento_resultante_m/s'])
+
+    df_mestre_limitado.loc[idx, 'Potência_Média'] = potencia_media_local
+
+    df_mestre_limitado.loc[idx, 'Altitude'] = df['Altitude_m'].iloc[0]
+
+  df_mestre_limitado.groupby('Pressão')
+
+  colunas_relevantes = ['Pressão', 'Altitude', 'Potência_Média']
+  df_mestre_limitado = df_mestre_limitado[colunas_relevantes]
+
+  df_mestre_agrupado = df_mestre_limitado.groupby('Pressão').agg({'Altitude': 'first', 'Potência_Média': 'sum'}).reset_index()
+  df_mestre_agrupado = df_mestre_agrupado.sort_values(by='Altitude')
+
+  print(df_mestre_agrupado)
