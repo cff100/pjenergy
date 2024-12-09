3f82599ff2e7d49689e16226a9925e037a73ec84
cff100 <caiofelix190799@gmail.com>
2024-12-09 16:19:38 +0000

    Atualizações no projeto

--- src/potencia/potencia_comparacao_alturas.py
+++ src/potencia/potencia_comparacao_alturas.py
@@ -12,32 +12,37 @@ def potencia_altura(perguntas, l_vel_inf, l_vel_sup, plotar_graficos):
   for an in range(2021, 2024):
     for est in ['Verão', 'Outono', 'Inverno', 'Primavera']:
       df_mestre_limitado = pd.concat([df_mestre_limitado, mp.pot(perguntas = False, pressao = 'Todas', estacao = est, ano = an, horario = 'Todos', plotar_graficos = plotar_graficos)])
+  print(f'df_mestre_limitado: {df_mestre_limitado}')
 
   for idx, df in enumerate(df_mestre_limitado['Dataframe_Probabilidade']):
 
     if not isinstance(df, pd.DataFrame):
-      print(f"Aviso: Elemento no índice {idx} não é um DataFrame, pulando...")
+      print(f"Aviso: Elemento no índice {idx} não é um DataFrame (df: {df}), pulando...")
       continue
 
-    df = df.loc[
+    df_limitado = df.loc[
         (df['Velocidade_Vento_resultante_m/s'] >= l_vel_inf) &
         (df['Velocidade_Vento_resultante_m/s'] <= l_vel_sup)
     ]
 
+    print(f'Índice: {idx} - df_limitado: {df_limitado}')
+
     # Calcular a potência
-    df['Potência'] = 0.5 * rho * A * (df['Velocidade_Vento_resultante_m/s'] ** 3) / 10**3  # Em kW
+    df_limitado['Potência'] = 0.5 * rho * A * (df_limitado['Velocidade_Vento_resultante_m/s'] ** 3) / 10**3  # Em kW
     # Calcular a potência ponderada
-    df['Potência_Ponderada'] = df['Potência'] * df['Densidade_de_Probabilidade']
+    df_limitado['Potência_Ponderada'] = df_limitado['Potência'] * df_limitado['Densidade_de_Probabilidade']
 
-    df = df.sort_values(by='Velocidade_Vento_resultante_m/s')
+    df_limitado = df.sort_values(by='Velocidade_Vento_resultante_m/s')
 
-    df_mestre_limitado.at[idx, 'Dataframe_Probabilidade'] = df
+    df_mestre_limitado.at[idx, 'Dataframe_Probabilidade'] = df_limitado
 
-    potencia_media_local = simps(df['Potência_Ponderada'], df['Velocidade_Vento_resultante_m/s'])
+    potencia_media_local = simps(df_limitado['Potência_Ponderada'], df_limitado['Velocidade_Vento_resultante_m/s'])
 
     df_mestre_limitado.loc[idx, 'Potência_Média'] = potencia_media_local
 
-    df_mestre_limitado.loc[idx, 'Altitude'] = df['Altitude_m'].iloc[0]
+    df_mestre_limitado.loc[idx, 'Altitude'] = df_limitado['Altitude_m'].iloc[0]
+
+  print(f'df_mestre_limitado: {df_mestre_agrupado}')
 
   df_mestre_limitado.groupby('Pressão')
 
