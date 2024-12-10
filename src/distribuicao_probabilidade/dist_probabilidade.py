d67008229b97a56549f2704a958b0f1e0b5e3f4e
cff100 <caiofelix190799@gmail.com>
2024-12-10 14:26:15 +0000

    Atualizações no projeto

--- src/distribuicao_probabilidade/dist_probabilidade.py
+++ src/distribuicao_probabilidade/dist_probabilidade.py
@@ -125,8 +125,8 @@ def usuario_weibull_velocidade(perguntas, pressao, estacao, ano, horario, exibir
 
   if perguntas == True:
     while aceito_1 == False:
-      valores_aceitos = list(range(972,1001)) + ['0', 'Todos']
-      valores_aceitos = [str(va) if va not in ('Todos', '0') else va for va in valores_aceitos]
+      valores_aceitos = list(range(972,1001)) + ['0', 'Todas']
+      valores_aceitos = [str(va) if va not in ('Todas', '0') else va for va in valores_aceitos]
       pressao = input('Qual pressão deseja observar (em HPa)? Escolha um número inteiro entre 972 e 1000. Escreva Todas ou 0 para não filtrar nenhuma pressão específica. \n')
       aceito_1 = vna.valores_nao_aceitos(pressao, valores_aceitos) # Verifica se é um valor aceito
       
@@ -208,7 +208,26 @@ def usuario_weibull_velocidade(perguntas, pressao, estacao, ano, horario, exibir
     print(f'Horário: {horario}')
 
   else:
-    pass
+    
+    valores_aceitos = list(range(972,1001)) + ['0', 'Todas']
+    valores_aceitos = [str(va) if va not in ('Todas', '0') else va for va in valores_aceitos]
+    aceito = vna.valores_nao_aceitos(pressao, valores_aceitos, dica = True, nome_variavel = 'pressão')
+    if aceito == False:
+      return None
+
+    aceito = vna.valores_nao_aceitos(estacao, ['Verão', 'Outono', 'Inverno', 'Primavera', 'Todas', '0'], dica = True, nome_variavel = 'estação')
+    if aceito == False:
+      return None
+
+    valores_aceitos = list(range(2010,2024)) + ['0', 'Todos']
+    valores_aceitos = [str(va) if va not in ('Todos', '0') else va for va in valores_aceitos]
+    aceito = vna.valores_nao_aceitos(ano, valores_aceitos, dica = True, nome_variavel = 'ano')
+    if aceito == False:
+      return None
+
+    aceito = vna.valores_nao_aceitos(horario, ['03:00', '09:00', '15:00', '21:00', 'Todos', '0'], dica = True, nome_variavel = 'horário')
+    if aceito == False:
+      return None
 
   tabela = plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico)
 
