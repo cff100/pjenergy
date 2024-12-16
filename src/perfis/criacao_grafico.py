4ee8a9e3acf23b522848f862ad6fba2e4d60e7f1
cff100 <caiofelix190799@gmail.com>
2024-12-16 10:58:42 +0000

    Atualizações no projeto

--- src/perfis/criacao_grafico.py
+++ src/perfis/criacao_grafico.py
@@ -6,7 +6,7 @@ Gera os gráficos de perfil de velocidade e tempertura.
 import matplotlib.pyplot as plt
 import numpy as np
 
-def criacao_grafico(ax, cor, Y, X, Y_smooth, X_smooth, grafico, plataforma, estacao, horario, variavel, componente_velocidade, data):
+def criacao_grafico(ax, cor, Y, X, Y_smooth, X_smooth, grafico, plataforma, estacao, horario, variavel, componente_velocidade, data, ano):
 
   horarios = ['03:00', '09:00', '15:00', '21:00']
 
@@ -21,13 +21,13 @@ def criacao_grafico(ax, cor, Y, X, Y_smooth, X_smooth, grafico, plataforma, esta
     # Configuração do título e do eixo X de acordo com o tipo de dado (velocidade ou temperatura)
     if variavel == 'velocidade':
       if data == None:
-        ax.set_title(f'Perfil de Velocidade do Vento - {plataforma} - Est: {estacao} - Dir: {componente_velocidade}')  # Define o título para perfis de velocidade
+        ax.set_title(f'Perfil de Velocidade do Vento - {plataforma} - Est: {estacao} - Ano: {ano} - Dir: {componente_velocidade}')  # Define o título para perfis de velocidade
       else:
         ax.set_title(f'Perfil de Velocidade do Vento - {plataforma} - Est: {estacao} - Data: {data} - Dir: {componente_velocidade}')  # Define o título para perfis de velocidade
       ax.set_xlabel('Velocidade do Vento (m/s)')  # Nome do eixo X para velocidade
     elif variavel == 'temperatura':
       if data == None:
-        ax.set_title(f'Perfil de Temperatura - {plataforma} - Est: {estacao}')  # Define o título para perfis de temperatura
+        ax.set_title(f'Perfil de Temperatura - {plataforma} - Est: {estacao} - Ano: {ano}')  # Define o título para perfis de temperatura
       else:
         ax.set_title(f'Perfil de Temperatura - {plataforma} - Est: {estacao} - Data: {data}')  # Define o título para perfis de temperatura
       ax.set_xlabel('Temperatura (°C)') # Nome do eixo X para temperatura
@@ -46,14 +46,14 @@ def criacao_grafico(ax, cor, Y, X, Y_smooth, X_smooth, grafico, plataforma, esta
     # Configuração do título e do eixo X de acordo com o tipo de dado (derivada de velocidade ou de temperatura)
     if variavel == 'velocidade':
       if data == None:
-        ax.set_title(f'Derivada do Perfil de Velocidade - {plataforma} - Est: {estacao}) - - Dir: {componente_velocidade}')  # Título para derivada de velocidade
+        ax.set_title(f'Derivada do Perfil de Velocidade - {plataforma} - Est: {estacao}) - Ano: {ano} - Dir: {componente_velocidade}')  # Título para derivada de velocidade
       else:
         ax.set_title(f'Derivada do Perfil de Velocidade - {plataforma} - Est: {estacao} - Data: {data} - - Dir: {componente_velocidade}')  # Título para derivada de velocidade
 
       ax.set_xlabel('Derivada da Velocidade do Vento')  # Nome do eixo X para derivada de velocidade
     elif variavel == 'temperatura':
       if data == None:
-        ax.set_title(f'Derivada do Perfil de Temperatura - {plataforma} - Est: {estacao}')  # Título para derivada de temperatura
+        ax.set_title(f'Derivada do Perfil de Temperatura - {plataforma} - Est: {estacao} - Ano: {ano}')  # Título para derivada de temperatura
       else:
         ax.set_title(f'Derivada do Perfil de Temperatura - {plataforma} - Est: {estacao} - Data: {data}')  # Título para derivada de temperatura
 
