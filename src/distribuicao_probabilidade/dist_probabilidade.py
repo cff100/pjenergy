a4c194203df2bd766e79024a5dda3a916d28a6c8
cff100 <caiofelix190799@gmail.com>
2024-12-17 20:49:08 +0000

    Atualizações no projeto

--- src/distribuicao_probabilidade/dist_probabilidade.py
+++ src/distribuicao_probabilidade/dist_probabilidade.py
@@ -1,13 +1,14 @@
-from ast import arguments
-from scipy.integrate import simps
+
 
 ''' INFO
 Gera um histograma e a distribuição de Weibull para a velocidade do vento,
 além de uma tabela com os pontos dessa distribuição.
 '''
 
-
+import src.auxiliares.traduzir_para_ingles as ti
 import src.auxiliares.respostas_usuario as ru
+from ast import arguments
+from scipy.integrate import simps
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -18,7 +19,7 @@ import warnings
 horarios = ['03:00', '09:00', '15:00', '21:00']
 plataforma_escolhida = "PETROBRAS XXXIII"
 
-def plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico):
+def plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico, ling_graf):
 
   # Lista de caminhos para os arquivos CSV
   arquivos_csv = ['/content/pjenergy/data/dados_interpolados/df_interpolado_Verao.csv',
@@ -98,20 +99,43 @@ def plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico):
     # Criar a figura
     fig, ax = plt.subplots(figsize=(10, 6))
 
+    if ling_graf == 'pt':
+      label_1 = 'Dados'
+      label_2 = 'Ajuste de Weibull'
+    elif ling_graf == 'en':
+      label1 = 'Data'
+      label_2 = 'Weibull Fit'
+      pressao = ti.trad_para_ingles(pressao)
+      estacao = ti.trad_para_ingles(estacao)
+      horario = ti.trad_para_ingles(horario)
+      ano = ti.trad_para_ingles(ano)
+
+
+    
     # Plotar o histograma
-    sns.histplot(velocidades, kde=False, stat='density', color='lightgray', alpha=0.5, bins=20, label='Dados')
+    sns.histplot(velocidades, kde=False, stat='density', color='lightgray', alpha=0.5, bins=20, label=label_1)
 
 
     # Plotar a curva ajustada
-    plt.plot(velocidades, weibull_pdf, label='Ajuste de Weibull', color='r', linewidth=2)
+    plt.plot(velocidades, weibull_pdf, label=label_2, color='r', linewidth=2)
 
-    ax.set_title(f'Histograma e Ajuste de Distribuição Weibull - Horário: {horario} - Pressão: {pressao} hPa - Estação: {estacao} - Ano: {ano}')
+    if ling_graf == 'pt':
+      ax.set_title(f'Histograma e Ajuste de Distribuição Weibull - Horário: {horario} - Pressão: {pressao} hPa - Estação: {estacao} - Ano: {ano}')
+    elif ling_graf == 'en':
+      ax.set_title(f'Histogram and Weibull Distribution Fit - Time: {horario} - Pressure: {pressao} hPa - Season: {estacao} - Year: {ano}')
     texto = plataforma_escolhida
-    ax.text(0.77, 0.85, f'Plataforma: {texto}', transform=ax.transAxes, fontsize=9, verticalalignment='top')
+    if ling_graf == 'pt':
+      ax.text(0.77, 0.85, f'Plataforma: {texto}', transform=ax.transAxes, fontsize=9, verticalalignment='top')
+    elif ling_graf == 'en':
+      ax.text(0.77, 0.85, f'Platform: {texto}', transform=ax.transAxes, fontsize=9, verticalalignment='top')
 
     # Configurações do gráfico
-    plt.xlabel('Velocidade do Vento (m/s)', fontsize=14)
-    plt.ylabel('Densidade de Probabilidade', fontsize=14)
+    if ling_graf == 'pt':
+      plt.xlabel('Velocidade do Vento (m/s)', fontsize=14)
+      plt.ylabel('Densidade de Probabilidade', fontsize=14)
+    elif ling_graf == 'en':
+      plt.xlabel('Wind Speed (m/s)', fontsize=14)
+      plt.ylabel('Probability Density', fontsize=14)
     plt.legend(fontsize=12)
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.tight_layout()
@@ -125,15 +149,15 @@ def plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico):
 
 
 
-def usuario_weibull_velocidade(perguntas, pressao, estacao, ano, horario, exibir_grafico):
+def usuario_weibull_velocidade(perguntas, pressao, estacao, ano, horario, exibir_grafico, ling_graf):
 
   '''Inicia a busca pelos argumentos do usuário'''
 
-  # Obtém e trata os argumentos de entrada do usuário 
+  # Obtém e trata os argumentos de entrada do usuário
   pressao, estacao, ano, horario = ru.resp_usuario_2(perguntas, pressao, estacao, ano, horario)
 
   # Chama a função que cria o dataframe com os valores da densidade de probabilidade
-  tabela = plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico)
+  tabela = plot_weibull_velocidade(pressao, estacao, ano, horario, exibir_grafico, ling_graf)
   tabela.reset_index(drop=True, inplace=True)
 
   return tabela
