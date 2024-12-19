b35decdb03b03b847a36155cbea236f38362fed6
cff100 <caiofelix190799@gmail.com>
2024-12-19 09:44:47 +0000

    Atualizações no projeto

--- src/potencia/grafico_potencia_ponderada.py
+++ src/potencia/grafico_potencia_ponderada.py
@@ -97,11 +97,8 @@ def pond_potencia(df_mestre, pressao_lista, estacao_lista, ano_lista, horario_li
     pressao_remover = [pressao_par for pressao_par in pressao_lista if pressao_par % 2 != 0]  
     df_mestre = df_mestre[~df_mestre['Pressão'].isin(pressao_remover)]
     print(f'1:{df_mestre}')
-      '''df_mestre = df_mestre.dropna(subset = ['Pressão'])
-      print(f'2:{df_mestre}')
-      df_mestre = df_mestre.reset_index(drop = True)
-      print(f'3:{df_mestre}')
-    #'''
+
+    
     
 
 
