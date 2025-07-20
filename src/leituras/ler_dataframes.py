d1d9b454659d07b4b4c737a6f10b5803eb0d325d
cff100 <caiofelix190799@gmail.com>
2025-07-20 07:42:25 -0300

    Organiza funções de leitura de dados

--- src/leituras/ler_dataframes.py
+++ src/leituras/ler_dataframes.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 import dask.dataframe as dd
 
+def ler_dataframe_parquet(diretorio: Path) -> dd.DataFrame:
+    """Lê dask dataframes no formato parquet.
+    
+    Args:
+        diretorio (Path): Diretório do dask dataframe.
 
+    Returns:
+        dd.Dataframe: Dataframe lido.
 
-def ler_dataframe_parquet(caminho: Path) -> dd.DataFrame:
+    Raises:
+        ValueError: Erro de leitura do dataframe.
+    """
     
     try:
-        df = dd.read_parquet(caminho)
+        df = dd.read_parquet(diretorio)
     except OSError as e:
         raise ValueError(f"Erro ao ler o dataframe: {e}")
 
--- src/leituras/ler_datasets.py
+++ src/leituras/ler_datasets.py
@@ -4,6 +4,17 @@ import xarray as xr
 
 
 def ler_dataset_nc(caminho: Path) -> xr.Dataset:
+    """Lê dataset no formato NetCDF.
+    
+    Args:
+        diretorio (Path): Diretório do dataset
+
+    Returns:
+        dd.Dataset: Dataset lido.
+
+    Raises:
+        ValueError: Erro de leitura do dataset.
+    """
     
     try:
         ds = xr.open_dataset(caminho, engine="netcdf4")
