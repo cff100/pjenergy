416e4979ad08a36e019d772e4b8daaaadc51bd37
cff100 <caiofelix190799@gmail.com>
2025-09-10 17:55:29 -0300

    Adiciona o resto das funções básicas de filtragem

--- src/edicoes/filtragem/filtragem.py
+++ src/edicoes/filtragem/filtragem.py
@@ -4,7 +4,8 @@ import dask.dataframe as dd
 from leituras.ler_arquivos_pastas_especificas import ler_dataframes_pontuais_plataformas_geral
 from config.constants import Correspondencias as cr
 
-def filtro(plataforma_representacao: str, coluna_de_filtragem: str, objetos_de_filtragem: Union[Any, list]) -> dd.DataFrame:
+
+def filtro_especifico(plataforma_representacao: str, coluna_de_filtragem: str, objetos_de_filtragem: Union[Any, list]) -> dd.DataFrame:
     """Filtra um dataframe por um objeto de filtragem.
     
     Args:
@@ -19,6 +20,27 @@ def filtro(plataforma_representacao: str, coluna_de_filtragem: str, objetos_de_f
     return df_filtrado
 
 
+def filtro_faixa_de_valores(plataforma_representacao: str, coluna_de_filtragem: str,
+                            maior_igual_a: Optional[int] = None, menor_igual_a: Optional[int] = None) -> dd.DataFrame:
+
+    df = ler_dataframes_pontuais_plataformas_geral(plataforma_representacao)
+    if maior_igual_a is not None and menor_igual_a is not None:
+        df_filtrado = df[(df[coluna_de_filtragem] >= maior_igual_a) & (df[coluna_de_filtragem] <= menor_igual_a)]
+    elif maior_igual_a is not None:
+        df_filtrado = df[df[coluna_de_filtragem] >= maior_igual_a]
+    elif menor_igual_a is not None:
+        df_filtrado = df[df[coluna_de_filtragem] <= menor_igual_a]
+    else:
+        df_filtrado = df
+
+    df_filtrado = cast(dd.DataFrame, df_filtrado)
+    return df_filtrado
+
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------
+
+
 def filtra_estacao(plataforma_representacao: str, estacoes: Union[str, list]) -> dd.DataFrame:
     """Filtra um dataframe por uma estação ou lista de estações.
     
@@ -27,7 +49,7 @@ def filtra_estacao(plataforma_representacao: str, estacoes: Union[str, list]) ->
         estacoes (str | list): Estação ou lista de estações pelas quais se deseja filtrar.
     """
 
-    df_filtrado = filtro(plataforma_representacao, cr.DadosVariaveis.ESTACAO_DO_ANO, estacoes )
+    df_filtrado = filtro_especifico(plataforma_representacao, cr.DadosVariaveis.ESTACAO_DO_ANO, estacoes )
 
     return df_filtrado
 
@@ -40,7 +62,7 @@ def filtra_ano(plataforma_representacao: str, anos: Union[int, list]) -> dd.Data
         anos (int | list): Ano ou lista de anos pelas quais se deseja filtrar.
     """
 
-    df_filtrado = filtro(plataforma_representacao, cr.DadosVariaveis.ANO, anos )
+    df_filtrado = filtro_especifico(plataforma_representacao, cr.DadosVariaveis.ANO, anos )
 
     return df_filtrado
 
@@ -53,9 +75,9 @@ def filtra_mes(plataforma_representacao: str, meses: Union[str, int, list]) -> d
         meses (str | int | list): Mês ou lista de meses pelos quais se deseja filtrar.
     """
     if ( isinstance(meses, list) and all(isinstance(mes, int) for mes in meses) ) or isinstance(meses, int):
-        df_filtrado = filtro(plataforma_representacao, cr.DadosVariaveis.MES, meses)
+        df_filtrado = filtro_especifico(plataforma_representacao, cr.DadosVariaveis.MES, meses)
     elif ( isinstance(meses, list) and all(isinstance(mes, str) for mes in meses) ) or isinstance(meses, str):
-        df_filtrado = filtro(plataforma_representacao, cr.DadosVariaveis.MES_STR, meses)
+        df_filtrado = filtro_especifico(plataforma_representacao, cr.DadosVariaveis.MES_STR, meses)
     else:
         raise TypeError("O(s) mes(es) que se deseja filtrar devem ser do tipo int, " \
         "str ou uma lista inteiramente de elementos int ou inteiramente de elementos str")
@@ -71,9 +93,9 @@ def filtra_hora(plataforma_representacao: str, horas: Union[str, int, list]) -> 
         horas (str | int | list): Hora ou lista de horas pelas quais se deseja filtrar.
     """
     if ( isinstance(horas, list) and all(isinstance(mes, int) for mes in horas) ) or isinstance(horas, int):
-        df_filtrado = filtro(plataforma_representacao, cr.DadosVariaveis.HORA, horas)
+        df_filtrado = filtro_especifico(plataforma_representacao, cr.DadosVariaveis.HORA, horas)
     elif ( isinstance(horas, list) and all(isinstance(mes, str) for mes in horas) ) or isinstance(horas, str):
-        df_filtrado = filtro(plataforma_representacao, cr.DadosVariaveis.HORA_STR, horas)
+        df_filtrado = filtro_especifico(plataforma_representacao, cr.DadosVariaveis.HORA_STR, horas)
     else:
         raise TypeError("A(s) hora(s) que se deseja filtrar devem ser do tipo int, " \
         "str ou uma lista inteiramente de elementos int ou inteiramente de elementos str")
@@ -96,18 +118,24 @@ def filtra_velocidade_vento(plataforma_representacao: str, componente: Literal["
         coluna_de_filtragem = cr.DadosVariaveis.VELOCIDADE_RESULTANTE
 
 
-    df = ler_dataframes_pontuais_plataformas_geral(plataforma_representacao)
-    if maior_igual_a is not None and menor_igual_a is not None:
-        df_filtrado = df[(df[coluna_de_filtragem] >= maior_igual_a) & (df[coluna_de_filtragem] <= menor_igual_a)]
-    elif maior_igual_a is not None:
-        df_filtrado = df[df[coluna_de_filtragem] >= maior_igual_a]
-    elif menor_igual_a is not None:
-        df_filtrado = df[df[coluna_de_filtragem] <= menor_igual_a]
-    else:
-        df_filtrado = df
+    df_filtrado = filtro_faixa_de_valores(plataforma_representacao, coluna_de_filtragem, 
+                                          maior_igual_a, menor_igual_a)
 
-    df_filtrado = cast(dd.DataFrame, df_filtrado)
     return df_filtrado
+
+
+def filtra_temperatura(plataforma_representacao: str, 
+                            maior_igual_a: Optional[int] = None, menor_igual_a: Optional[int] = None) -> dd.DataFrame:
+
+    """Filtra um dataframe por valores de temperatura do ar.
+    """
+    coluna_de_filtragem = cr.DadosVariaveis.TEMPERATURA_CELSIUS
+
+    df_filtrado = filtro_faixa_de_valores(plataforma_representacao, coluna_de_filtragem, 
+                                          maior_igual_a, menor_igual_a)
+    
+    return df_filtrado
+
 
 
 if __name__ == "__main__":
@@ -115,15 +143,26 @@ if __name__ == "__main__":
     import pandas as pd
     pd.set_option('display.max_columns', None)
 
-    #df_filtrado = filtra_ano("p7", [2020, 2024, 2017])
-    #df_filtrado = filtra_estacao("p7", ["Outono", "Inverno"])
-    #df_filtrado = filtra_mes("p7", [8, 4])
-    #df_filtrado = filtra_hora("p7", [6])
+    df_filtrado = filtra_ano("p7", [2020, 2024, 2017])
+    print(f'Filtrado por anos: {df_filtrado.compute()}\n')
 
-    df_filtrado = filtra_velocidade_vento("p7")
-    print(df_filtrado["vel_res"].compute().min())
-    print(df_filtrado["vel_res"].compute().max())
-    df_filtrado = filtra_velocidade_vento("p7", menor_igual_a = 1)
-    print(df_filtrado.compute())
-    print(df_filtrado.dtypes)
+    df_filtrado = filtra_estacao("p7", ["Outono", "Inverno"])
+    print(f'Filtrado por estações: {df_filtrado.compute()}\n')
+
+    df_filtrado = filtra_mes("p7", [8, 4])
+    print(f'Filtrado por meses: {df_filtrado.compute()}\n')
+
+    df_filtrado = filtra_hora("p7", [6])
+    print(f'Filtrado por horas: {df_filtrado.compute()}\n')
+
+    df_filtrado = filtra_velocidade_vento("p7", menor_igual_a = 14)
+    print(f'Filtrado por velocidades do vento: {df_filtrado.compute()}\n')
+
+    #print(df_filtrado["t_C"].compute().min())
+    #print(df_filtrado["t_C"].compute().max())
+
+    df_filtrado = filtra_temperatura("p7", maior_igual_a = 25)
+    print(f'Filtrado por temperaturas: {df_filtrado.compute()}\n')
+
+    print(f'Tipos: {df_filtrado.dtypes}\n')
     
