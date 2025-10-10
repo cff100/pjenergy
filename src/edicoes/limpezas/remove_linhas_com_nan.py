af638b41a42dc0753b5741b401bfc0765d313371
cff100 <caiofelix190799@gmail.com>
2025-10-10 09:59:41 -0300

    Adição de docstrings

--- src/edicoes/limpezas/remove_linhas_com_nan.py
+++ src/edicoes/limpezas/remove_linhas_com_nan.py
@@ -1,6 +1,9 @@
 import dask.dataframe as dd
 
 def remove_linhas_nan_dataframes(df: dd.DataFrame) -> dd.DataFrame:
+    """
+    Remove linhas com valores NaN do dataframe.
+    """
 
     df = df.dropna()
     return df
--- src/utils/encontra_nan.py
+++ src/utils/encontra_nan.py
@@ -1,37 +1,23 @@
-
-# import xarray as xr
 import dask.dataframe as dd 
-# from pathlib import Path
+
 
 # from leituras.ler_datasets import ler_dataset_nc
 
 def linhas_dataframes_com_nan(df: dd.DataFrame) -> tuple[dd.DataFrame, dd.DataFrame]:
-    """Filtra para mostrar as linhas com algum valor NaN"""
+    """
+    Filtra para mostrar as linhas com algum valor NaN e também calcula quantos elementos NaN há em cada coluna.
+    """
 
     df_nan = df[df.isna().any(axis=1)]
     n_valores = df.isna().sum()
     return df_nan, n_valores
 
-# def elementos_dataset_com_nan(caminho: Path) -> xr.Dataset:
 
-#     ds = ler_dataset_nc(caminho)
-#     print(ds)
-#     mask = ds.to_array().isnull().any(dim=('variable', 'latitude', 'longitude'))
-#     ds_com_faltantes = ds.sel(valid_time=mask)
-
-#     return ds_com_faltantes
 
 
 if __name__ == "__main__":
-    from leituras.ler_arquivos_pastas_especificas import ler_dataframes_pontuais_plataformas_geral
-    
-    df = ler_dataframes_pontuais_plataformas_geral("p2")
+    from leituras.ler_arquivos_pastas_especificas import ler_dataframes_pontuais_plataformas_geral  
+    df = ler_dataframes_pontuais_plataformas_geral("p7")
     df_nan, n_valores = linhas_dataframes_com_nan(df)
     print(n_valores.compute())
     print(df_nan.compute().sample(15))
-
-
-    # from config.paths import DiretoriosBasicos as db
-
-    # ds_nan = elementos_dataset_com_nan(db.DIRETORIO_DADOS / "datasets/originais/(var-geopotential)_(ano-2015)_(pressao-900).nc")
-    # print(ds_nan)
