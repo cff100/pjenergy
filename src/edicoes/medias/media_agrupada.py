eede6654fea0b5888dabc33fcca5231e4df06b39
cff100 <caiofelix190799@gmail.com>
2025-10-12 09:37:04 -0300

    Adição de comentários à função de geração de médias em dataframes

--- src/edicoes/medias/media_agrupada.py
+++ src/edicoes/medias/media_agrupada.py
@@ -1,20 +1,37 @@
 import dask.dataframe as dd
 from typing import cast
 
-def media_agrupada(df: dd.DataFrame, colunas_remover: list, categoria_de_agrupamento: list = ["data_bras", "h"], colunas_alvo: list = ["vel_u","vel_v","vel_res","t_C","t_K","r"]) -> dd.DataFrame:
+def media_agrupada(df: dd.DataFrame, colunas_remover: list, 
+                   categoria_de_agrupamento: list = ["data_bras", "h"], 
+                   colunas_alvo: list = ["vel_u","vel_v","vel_res","t_C","t_K","r"]) -> dd.DataFrame:
+    """
+    Função geral para fazer média agrupada de valores de um dataframe.
 
+    Args:
+        df (dd.Dataframe): Dataframe objeto da média
+        colunas_remover (list): Lista com os nomes das colunas a serem removidas. 
+            Por exemplo, em uma média diária, a coluna de horas se torna inútil e deve ser eliminada.
+        categorias_de_agrupamento (lista): Lista com as categorias em relação às quais agrupar
+        colunas_alvos (lista): Lista com os nomes das colunas com valores que deseja se fazer a média.
 
-    df = df.drop(columns=["tempo_UTC0"], errors="ignore")
+    """
+
+    # Remove colunas desnecessárias
     df = df.drop(columns=colunas_remover, errors="ignore")
+    # Remove a coluna de tempo em UTC0, já que as médias são feitas considerando o tempo no horário de Brasília, 
+    # tornando os valores em outros fusos horários incompatíveis.
+    df = df.drop(columns=["tempo_UTC0"], errors="ignore")  
 
     #print(df.compute())
 
     todas_colunas = list(df.columns)
-    colunas_nao_alvo = [coluna for coluna in todas_colunas if coluna not in colunas_alvo]
+    colunas_nao_alvo = [coluna for coluna in todas_colunas if coluna not in colunas_alvo] # Lista de colunas que não se deseja vazer média
 
+    # Criação de dicionário com instruções para o método de média agrupada
     agg_dicio = {col: "mean" for col in colunas_alvo}
     agg_dicio.update({col: "first" for col in colunas_nao_alvo})
 
+    # Cálculo da média agrupada
     media = df.groupby(categoria_de_agrupamento, sort=categoria_de_agrupamento).agg(agg_dicio)
     media = cast(dd.DataFrame, media)
     
