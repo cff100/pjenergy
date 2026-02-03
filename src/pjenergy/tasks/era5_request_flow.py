a4a33a4f2ef3d3e44af875eaf314382887e57855
cff100 <caiofelix190799@gmail.com>
2026-02-03 09:29:50 -0300

    Pequenas modificações

--- src/pjenergy/tasks/era5_request_flow.py
+++ src/pjenergy/tasks/era5_request_flow.py
@@ -5,7 +5,7 @@ from pjenergy.config.constants import RequestFlowConstants
 def limit_selection_flow(limit: int = RequestFlowConstants.DEFAULT_REQUEST_LIMIT, 
                          force: bool = False):
     
-    limit = ask_request_limit(limit)
+    #limit = ask_request_limit(limit)
 
     if limit >= RequestFlowConstants.MEDIUM_REQUET_LIMIT and not force:
         limit = ask_alternative_combination_limit(limit)
